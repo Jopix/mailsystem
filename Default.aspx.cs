73c650b62012b9379303a0cc9672d59771987c93
Jopix <Jopix@JopixChendeMacBook-Pro.local>
2015-05-16 13:48:08 +0800

    优化。

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -5,17 +5,21 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+private static int count = 1; 
+
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
-        if (Int32.Parse(Application["UserCount"].ToString()) % 2 == 0 ) 
+        count ++;
+        if (count % 2 == 0 ) 
         {
             onlineNum.Text = "我是假的 特码：1 = 100元， 4 = 500 元， 7 = 100元，69 = 10元";
         }
         else{
             onlineNum.Text = "我是假的 特码：1 = 100元， 4 = 500 元，2 = 100000000000元，7 = 100元，69 = 10元";
         }
+        count = count % 2;
     }
 }
